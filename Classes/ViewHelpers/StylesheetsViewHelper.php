07925b4167d321e1b4b0a687557d864a4dc24297
Claus Due <claus@namelesscoder.net>
2015-03-04 16:14:52 +0100

    [TASK] Include Bootstrap CSS when TYPO3 core version is <7.0.0
    Summary:
    Includes the CSS resources on 6.2 which are natively
    included on 7.0.0. Does so via a ViewHelper that returns the
    appropriate array of CSS files to include.
    
    Also configured arc linting to exclude bootstrap directory.
    
    Test Plan: apply patch, load backend module in 6.2.
    
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D953

---
+++ Classes/ViewHelpers/StylesheetsViewHelper.php
@@ -0,0 +1,48 @@
+<?php
+namespace Dkd\Contentdashboard\ViewHelpers;
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class StylesheetsViewHelper
+ */
+class StylesheetsViewHelper extends AbstractViewHelper {
+
+	/**
+	 * Returns an array (for feeding into f:be.container) of
+	 * stylesheet files that should be loaded. Will load an
+	 * additional Bootstrap resource if TYPO3 version is
+	 * below 7.0 which includes this resource natively.
+	 *
+	 * @return array
+	 */
+	public function render() {
+		$path = $this->getPublicResourcePath();
+		$stylesheets = array(
+			$path . 'Stylesheets/Default.css'
+		);
+		if (TRUE === $this->isCoreVersionBelowSeven()) {
+			$stylesheets[] = $path . 'Themes/Bootstrap/css/bootstrap.min.css';
+		}
+		return $stylesheets;
+	}
+
+	/**
+	 * @codeCoverageIgnore
+	 * @return boolean
+	 */
+	protected function isCoreVersionBelowSeven() {
+		return version_compare(VersionNumberUtility::getNumericTypo3Version(), '7.1.0', '<');
+	}
+
+	/**
+	 * @codeCoverageIgnore
+	 * @return string
+	 */
+	protected function getPublicResourcePath() {
+		return '../' . ExtensionManagementUtility::siteRelPath('contentdashboard') . 'Resources/Public/';
+	}
+
+}
